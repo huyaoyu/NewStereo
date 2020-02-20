588a8beef2811e47bd35e6d5afeb0df1a6a783e9
Yaoyu Hu <huyaoyu1986@gmail.com>
2020-02-20 01:09:29 -0500

    RC.

--- Trial/RecurrentStereo/Model/ImageStack.py
+++ Trial/RecurrentStereo/Model/ImageStack.py
@@ -1,6 +1,5 @@
 import torch
 
-
 def stack_single_channel_tensor(img, shift=16, radius=32):
     """
     img is a tensor which is obtained by coverting a signle channel NumPy 
@@ -17,3 +16,72 @@ def stack_single_channel_tensor(img, shift=16, radius=32):
     is firstly padded by radius*shift pixels and the padded value is zero. And the shifted
     images are sliced from the padded image with the size of the original image (H, W).
     """
+
+    # Argument check.
+    assert shift >= 0
+    assert radius >= 0
+
+    # Force the arguments to be integers.
+    shift  = int(shift)
+    radius = int(radius)
+
+    # Dimensions.
+    B, C, H, W = img.size()
+
+    assert C == 1
+    
+    padRadius = radius * shift
+    paddedH   = H # Only pad the x-axis.
+    paddedW   = W + 2*padRadius
+
+    # Create a temporary tensor.
+    paddedImg = torch.zeros( ( B, 1, paddedH, paddedW ), dtype=torch.float32 )
+    paddedImg.requires_grad = False
+
+    # Copy data from img to paddedImg.
+    paddedImg[:, :, :, padRadius:padRadius+W] = img.detach().clone()
+
+    # Create the output tensor.
+    C = 2*radius + 1
+    out = torch.zeros( (B, C, H, W), dtype=torch.float32 )
+
+    # Temporarily disable the gradient.
+    out.requires_grad = False
+
+    # Copy data to the output tensor.
+    for i in range(C):
+        idxH = 0
+        idxW = i * shift
+
+        # Get the channel.
+        out[:, i, :, :] = paddedImg[:, 0, idxH:idxH+H, idxW:idxW+W]
+
+    # Enable the gradient.
+    out.requires_grad = True
+
+    return out
+
+if __name__ == "__main__":
+    print("Test ImageStack.py")
+
+    row = torch.arange(1, 33)
+
+    img = row.repeat((2,1,32,1))
+    img[1, 0, :, :] = img[1, 0, :, :] + 100
+    # import ipdb; ipdb.set_trace()
+
+    print("img.size() = {}. ".format(img.size()))
+    print("img[0, 0, :, :] = \n{}".format( img[0,0,:,:] ))
+    print("img[1, 0, :, :] = \n{}".format( img[1,0,:,:] ))
+
+    stacked = stack_single_channel_tensor(img, shift=2, radius=8)
+    print("stacked.size() = {}. ".format(stacked.size()))
+
+    print("stacked[0, 0, :, :]  = \n{}".format( stacked[0, 0, :, :] ))
+    print("stacked[0, 8, :, :]  = \n{}".format( stacked[0, 8, :, :] ))
+    print("stacked[0, 16, :, :] = \n{}".format( stacked[0, 16, :, :] ))
+
+    print("stacked[1, 0, :, :]  = \n{}".format( stacked[1, 0, :, :] ))
+    print("stacked[1, 8, :, :]  = \n{}".format( stacked[1, 8, :, :] ))
+    print("stacked[1, 16, :, :] = \n{}".format( stacked[1, 16, :, :] ))
+    
